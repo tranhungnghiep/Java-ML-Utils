7aab7baec26d21ba24996b44dae180d321bf978b
Hung Nghiep Tran <tranhungnghiep@gmail.com>
2017-01-16 08:43:38 +0900

    Create InternetUtility.

---
+++ src/main/java/thn/research/textutility/general/InternetUtility.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thn.research.textutility.general;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author mac
+ */
+public class InternetUtility {
+
+    /**
+     * Follow url redirect, return the final url. Also work with http/https switch. Consecutive redirect supported.
+     * 
+     * @param url
+     * @param connectionTimeout
+     * @param readTimeout
+     * @return
+     * @throws Exception 
+     */
+    public static String getFinalRedirectURL(String url, int connectionTimeout, int readTimeout) throws Exception {
+        URL u = new URL(url);
+        HttpURLConnection httpConn = (HttpURLConnection) u.openConnection();
+        httpConn.setConnectTimeout(connectionTimeout);
+        httpConn.setReadTimeout(readTimeout);
+        httpConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+
+        httpConn.setInstanceFollowRedirects(false);
+        int status = httpConn.getResponseCode();
+        if (status == HttpURLConnection.HTTP_MOVED_PERM
+			|| status == HttpURLConnection.HTTP_MOVED_TEMP
+				|| status == HttpURLConnection.HTTP_SEE_OTHER) {
+            String redirectUrl = httpConn.getHeaderField("Location");
+            if (redirectUrl.startsWith("/")) {
+                if (u.getPort() == -1) {
+                    redirectUrl = u.getProtocol() + "://" + u.getHost() + redirectUrl;
+                } else {
+                    redirectUrl = u.getProtocol() + "://" + u.getHost() + ":" + u.getPort() + redirectUrl;
+                }
+            }
+            return getFinalRedirectURL(redirectUrl, connectionTimeout, readTimeout);
+        }
+        return url;
+    }
+}
