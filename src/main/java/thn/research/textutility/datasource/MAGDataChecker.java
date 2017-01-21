f6b35ab1991c5f93388d4867754c907583786bac
Hung Nghiep Tran <tranhungnghiep@gmail.com>
2017-01-21 09:29:08 +0900

    Checker.

---
+++ src/main/java/thn/research/textutility/datasource/MAGDataChecker.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thn.research.textutility.datasource;
+
+/**
+ * 1. Check if there are papers with same title && year.
+ * 2. Check if papers in arxiv/citeseerx are included in core fulltext.
+ * 
+ * @author THNghiep
+ */
+public class MAGDataChecker {
+    
+}
